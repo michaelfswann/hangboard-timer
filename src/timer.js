814cebf544291529c8bec0b7dfded14d60486fcb
= <=>
2021-01-27 19:24:56 +0000

    adding phases to timer

--- src/timer.js
+++ src/timer.js
@@ -1,16 +1,21 @@
 import React, { useEffect, useState } from 'react'
 
-const exampleInput = {rest:120, hang:5, sets:6, prep: 15}
+const exampleInput = {rest:120, hang:5, sets:6, prep: 4}
 
 const Timer = () => {
-    const [timer, setTimer] = useState({...exampleInput, activePhase: ''})
-    const [countdown, setCountdown] = useState(4)
+    const [timer, setTimer] = useState({...exampleInput, phase:1})
+    const [countdown, setCountdown] = useState(exampleInput.prep)
     
     useEffect( ()=>{
     const x = setInterval(()=> {
         if (countdown > 0) {
         setCountdown(countdown-1)
-    }}, 1000)
+    } else if (timer.phase !==2) {
+        setTimer({...timer, phase: 2})
+    }
+
+
+}, 1000)
     return () => {clearInterval(x)} })
 
 
