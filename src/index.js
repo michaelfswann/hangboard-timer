0afb5e0928460b52585cb639ffa0294d93b8d21e
michaelfswann <mikeswann@mail.com>
2021-01-27 00:37:13 +0000

    react working without boilerplate

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { render } from "react-dom";
+
+import Input from "./input";
+
+render(
+  <div className="app">
+    <p>Building...</p>
+    <Input />
+  </div>,
+  document.getElementById("root")
+);
---
+++ src/input.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Input = () => {
+  return (
+    <div>
+      <input type="number" name="seconds"></input>
+    </div>
+  );
+};
+
+export default Input;
+
+<input
+  type="number"
+  id="input-rest-length"
+  name="input-rest-length"
+  min="1"
+  max="180"
+  value="120"
+/>;
