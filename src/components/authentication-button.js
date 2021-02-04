5461aad97e83b30eddf4db2a04e5b2edb13ad483
michaelfswann <mikeswann@mail.com>
2021-02-04 20:27:31 +0000

    removed useAudio from timer

--- src/components/authentication-button.js
+++ src/components/authentication-button.js
@@ -7,7 +7,6 @@ import { useAuth0 } from '@auth0/auth0-react';
 
 const AuthenticationButton = () => {
   const { isAuthenticated } = useAuth0();
-  console.log(isAuthenticated);
 
   return isAuthenticated ? <LogoutButton /> : <LoginButton />;
 };
