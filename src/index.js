815209541de646668571e6b18bc224c77b56d258
michaelfswann <mikeswann@mail.com>
2021-02-04 14:58:03 +0000

    fixed import bug

--- src/index.js
+++ src/index.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Auth0Provider } from '@auth0/auth0-react';
-
-const DOMAIN = process.env.REACT_APP_AUTH0_DOMAIN;
-const CLIENTID = process.env.REACT_APP_AUTH0_CLIENT_ID;
+import Auth0ProviderWithHistory from './auth/auth0-provider-with-history';
 
 import Application from './application';
 //import Timer from './timer'
 render(
-  <Auth0Provider
-    domain={DOMAIN}
-    clientId={CLIENTID}
-    redirectUri={window.location.origin}
-  >
+  <Auth0ProviderWithHistory>
     <div className="app">
       <Application />
     </div>
-  </Auth0Provider>,
+  </Auth0ProviderWithHistory>,
   document.getElementById('root')
 );
