a0276fb6a6f4ee12a1e51dd6c3af9b4158715679
michaelfswann <mikeswann@mail.com>
2021-02-21 16:45:05 +0000

    home pages

--- src/components/main-nav.js
+++ src/components/main-nav.js
@@ -3,22 +3,26 @@ import React from 'react';
 
 const MainNav = () => (
   <div className="nav-bar-links">
-    <NavLink
-      to="/"
-      exact
-      className="nav-link"
-      activeClassName="router-link-exact-active"
-    >
-      Home
-    </NavLink>
-    <NavLink
-      to="/data"
-      exact
-      className="nav-link"
-      activeClassName="router-link-exact-active"
-    >
-      Data
-    </NavLink>
+    <div className="link">
+      <NavLink
+        to="/"
+        exact
+        className="nav-link"
+        activeClassName="router-link-exact-active"
+      >
+        Home
+      </NavLink>
+    </div>
+    <div className="link">
+      <NavLink
+        to="/data"
+        exact
+        className="nav-link"
+        activeClassName="router-link-exact-active"
+      >
+        Data
+      </NavLink>
+    </div>
   </div>
 );
 
